d8e68380b829af785c062a35a810195ff21563ce
Antonio Song <yuhanson@andrew.cmu.edu>
2019-02-22 23:05:51 -0500

    its working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ApplicationRecord
 	has_many :photos
+	has_one :article
 
 	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+  	scope :alphabetical, -> { order('name') }
   
 end
